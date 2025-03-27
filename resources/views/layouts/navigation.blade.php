e3469aea0f259fd6511355fd0b100885d7b526df
DSA <dsa@mtb111.fr>
2025-03-27 11:16:40 +0100

    we

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,11 +12,9 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-<<<<<<< HEAD
                     <x-nav-link :href="route('films.index')" :active="request()->routeIs('films.index')">
                         {{ __('Films') }}
                     </x-nav-link>
-=======
                     <!-- <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link> -->
@@ -24,7 +22,6 @@
                         {{ __('Accueil') }}
                     </x-nav-link>
 
->>>>>>> a7da741f8cccd78179be3aa76fc445a4adf643c9
                 </div>
             </div>
 
@@ -51,10 +48,6 @@
                         <!-- Authentication -->
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
-<<<<<<< HEAD
-=======
-
->>>>>>> a7da741f8cccd78179be3aa76fc445a4adf643c9
                             <x-dropdown-link :href="route('logout')"
                                     onclick="event.preventDefault();
                                                 this.closest('form').submit();">
@@ -65,15 +58,9 @@
                 </x-dropdown>
             </div>
 
-<<<<<<< HEAD
             <!-- Hamburger Menu (Mobile) -->
             <div class="-me-2 flex items-center sm:hidden">
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none transition duration-150 ease-in-out">
-=======
-            <!-- Hamburger -->
-            <div class="-me-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
->>>>>>> a7da741f8cccd78179be3aa76fc445a4adf643c9
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                         <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
@@ -85,16 +72,6 @@
 
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-<<<<<<< HEAD
-=======
-        <!-- <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
-        </div> -->
-
-        <!-- Responsive Settings Options -->
->>>>>>> a7da741f8cccd78179be3aa76fc445a4adf643c9
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
             <div class="px-4">
                 <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
@@ -106,7 +83,6 @@
                     {{ __('Profile') }}
                 </x-responsive-nav-link>
 
-<<<<<<< HEAD
                 <x-responsive-nav-link :href="route('films.index')">
                     {{ __('Films') }}
                 </x-responsive-nav-link>
@@ -114,12 +90,6 @@
                 <!-- Authentication -->
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
-=======
-                <!-- Authentication -->
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-
->>>>>>> a7da741f8cccd78179be3aa76fc445a4adf643c9
                     <x-responsive-nav-link :href="route('logout')"
                             onclick="event.preventDefault();
                                         this.closest('form').submit();">
