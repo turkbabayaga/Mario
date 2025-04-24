d50a4f7405d8387c69cd2179798926cf6f0d2e5e
turccdeter <dsa@mtb111.fr>
2025-04-24 09:57:00 +0200

    fix : retirer flèche inutile

--- resources/views/stocks/index.blade.php
+++ resources/views/stocks/index.blade.php
@@ -24,7 +24,6 @@
                                 @endforeach
                             </select>
                             <div class="pointer-events-none absolute inset-y-0 right-3 flex items-center">
-                                <svg class="w-4 h-4 text-gray-500" fill="currentColor" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.293l3.71-4.06a.75.75 0 011.08 1.04l-4.25 4.65a.75.75 0 01-1.08 0L5.21 8.27a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
                                 </svg>
                             </div>
